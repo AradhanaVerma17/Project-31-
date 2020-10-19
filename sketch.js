db41c9a42bcf56d1400c003bdbc01fd55ea9301b
AradhanaVerma17 <70834632+AradhanaVerma17@users.noreply.github.com>
2020-10-19 15:12:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var Engine = Matter.Engine,
+World = Matter.World,
+Events =  Matter.Events,
+Bodies =  Matter.Bodies,
+
+var particles = [];
+var  plinkos = [];
+var divisisions = [];
+var divisisionsHeight = 300;
+var score = [];
+
+function setup() {
+  createCanvas(800,800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2, height,width,20);
+}
+
+
+for (var k = 0; k<=width; k=k +80) {
+divisisions.push(new Divisisions(k, height-divisisionHeight/2,10,divisisionHeight));
+}
+
+for(var j = 75; j <=width; j=j+50)
+{
+  plinkos.push(new plinkos(j,75));
+}
+
+for(var j = 50; j <=width-10; j=j+50)
+{
+  plinkos.push(new plinkos(j,175));
+}
+
+function draw() {
+  background("black");  
+  textSize(20)
+  //text("Score i "+score,20,30);
+  Engine.update(engine);
+  ground.display();
+
+for(var i = 0; i<plinkos.length;i++ ){
+  plinkos[i].display();
+}
+if(frameCount%60===0){
+  particles.push(new particles(random(width/2-10,width/2+30),10,10));
+  score++;
+}
+
+for (var j = 0; k <particles.length;i++){
+  particles[j].display();
+}
+for(var k = 0; k <divisisions.length;k++){
+
+  divisisions[k].display();
+}
+
+  drawSprites();
+}
